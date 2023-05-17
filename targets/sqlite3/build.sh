6c60fb061f89c6412129189aa4c9f4ff5f952924
Andrea Jemmett <1787979+acidghost@users.noreply.github.com>
2023-05-17 12:28:24 +0200

    fix(sqlite3): disable amalgamation

--- targets/sqlite3/build.sh
+++ targets/sqlite3/build.sh
@@ -21,19 +21,20 @@ rm -rf "$WORK"
 mkdir -p "$WORK"
 cd "$WORK"
 
-export CFLAGS="$CFLAGS -DSQLITE_MAX_LENGTH=128000000 \
+export CFLAGS="$CFLAGS -Wno-all \
+               -DSQLITE_MAX_LENGTH=128000000 \
                -DSQLITE_MAX_SQL_LENGTH=128000000 \
                -DSQLITE_MAX_MEMORY=25000000 \
                -DSQLITE_PRINTF_PRECISION_LIMIT=1048576 \
                -DSQLITE_DEBUG=1 \
                -DSQLITE_MAX_PAGE_COUNT=16384"
 
-"$TARGET/repo"/configure --disable-shared --enable-rtree
+"$TARGET/repo"/configure --disable-shared --disable-amalgamation --enable-rtree
 make clean
 make -j$(nproc)
-make sqlite3.c
 
 $CC $CFLAGS -I. \
-    "$TARGET/repo/test/ossfuzz.c" "./sqlite3.o" \
+    "$TARGET/repo/test/ossfuzz.c" \
     -o "$OUT/sqlite3_fuzz" \
+    .libs/libsqlite3.a \
     $LDFLAGS $LIBS -pthread -ldl -lm
