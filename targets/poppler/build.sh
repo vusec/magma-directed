58871d4a03c01ab8c476c4e06860da7112f8acab
Andrea Jemmett <1787979+acidghost@users.noreply.github.com>
2023-06-03 20:24:13 +0200

    feat(poppler): build select drivers

--- targets/poppler/build.sh
+++ targets/poppler/build.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-set -e
+#!/bin/bash -ex
 
 ##
 # Pre-requirements:
@@ -11,6 +10,10 @@ set -e
 if [ ! -d "$TARGET/repo" ]; then
     echo "fetch.sh must be executed first."
     exit 1
+fi
+
+if [ -f "$FUZZER/configrc" ]; then
+    source "$FUZZER/configrc"
 fi
 
 export WORK="$TARGET/work"
@@ -34,36 +37,56 @@ test -n "$AR" && EXTRA="$EXTRA -DCMAKE_AR=$AR"
 test -n "$RANLIB" && EXTRA="$EXTRA -DCMAKE_RANLIB=$RANLIB"
 
 cmake "$TARGET/repo" \
-  $EXTRA \
-  -DCMAKE_BUILD_TYPE=debug \
-  -DBUILD_SHARED_LIBS=OFF \
-  -DFONT_CONFIGURATION=generic \
-  -DBUILD_GTK_TESTS=OFF \
-  -DBUILD_QT5_TESTS=OFF \
-  -DBUILD_CPP_TESTS=OFF \
-  -DENABLE_LIBPNG=ON \
-  -DENABLE_LIBTIFF=ON \
-  -DENABLE_LIBJPEG=ON \
-  -DENABLE_SPLASH=ON \
-  -DENABLE_UTILS=ON \
-  -DWITH_Cairo=ON \
-  -DENABLE_CMS=none \
-  -DENABLE_LIBCURL=OFF \
-  -DENABLE_GLIB=OFF \
-  -DENABLE_GOBJECT_INTROSPECTION=OFF \
-  -DENABLE_QT5=OFF \
-  -DENABLE_LIBCURL=OFF \
-  -DWITH_NSS3=OFF \
-  -DFREETYPE_INCLUDE_DIRS="$WORK/include/freetype2" \
-  -DFREETYPE_LIBRARY="$WORK/lib/libfreetype.a" \
-  -DICONV_LIBRARIES="/usr/lib/x86_64-linux-gnu/libc.so" \
-  -DCMAKE_EXE_LINKER_FLAGS_INIT="$LIBS"
-make -j$(nproc) poppler poppler-cpp pdfimages pdftoppm
+    $EXTRA \
+    -DCMAKE_BUILD_TYPE=debug \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DFONT_CONFIGURATION=generic \
+    -DBUILD_GTK_TESTS=OFF \
+    -DBUILD_QT5_TESTS=OFF \
+    -DBUILD_CPP_TESTS=OFF \
+    -DENABLE_LIBPNG=ON \
+    -DENABLE_LIBTIFF=ON \
+    -DENABLE_LIBJPEG=ON \
+    -DENABLE_SPLASH=ON \
+    -DENABLE_UTILS=ON \
+    -DWITH_Cairo=ON \
+    -DENABLE_CMS=none \
+    -DENABLE_LIBCURL=OFF \
+    -DENABLE_GLIB=OFF \
+    -DENABLE_GOBJECT_INTROSPECTION=OFF \
+    -DENABLE_QT5=OFF \
+    -DENABLE_LIBCURL=OFF \
+    -DWITH_NSS3=OFF \
+    -DFREETYPE_INCLUDE_DIRS="$WORK/include/freetype2" \
+    -DFREETYPE_LIBRARY="$WORK/lib/libfreetype.a" \
+    -DICONV_LIBRARIES="/usr/lib/x86_64-linux-gnu/libc.so" \
+    -DCMAKE_EXE_LINKER_FLAGS_INIT="$LIBS"
+
+if [ "${#poppler_BUILD_PROGRAMS[@]}" -eq 0 ]; then
+    poppler_BUILD_PROGRAMS=(pdftoppm pdfimages pdf_fuzzer)
+fi
+
+programs=()
+for program in "${poppler_BUILD_PROGRAMS[@]}"; do
+    if [[ $program =~ ^(pdfimages|pdftoppm)$ ]]; then
+        programs+=("$program")
+        poppler_BUILD_PROGRAMS=("${poppler_BUILD_PROGRAMS[@]/$program/}")
+    fi
+done
+
+make -j$(nproc) poppler poppler-cpp "${programs[@]}"
 EXTRA=""
 
-cp "$WORK/poppler/utils/"{pdfimages,pdftoppm} "$OUT/"
-$CXX $CXXFLAGS -std=c++11 -I"$WORK/poppler/cpp" -I"$TARGET/repo/cpp" \
-    "$TARGET/src/pdf_fuzzer.cc" -o "$OUT/pdf_fuzzer" \
-    "$WORK/poppler/cpp/libpoppler-cpp.a" "$WORK/poppler/libpoppler.a" \
-    "$WORK/lib/libfreetype.a" $LDFLAGS $LIBS -ljpeg -lz \
-    -lopenjp2 -lpng -ltiff -llcms2 -lm -lpthread -pthread
+for program in "${programs[@]}"; do
+    cp "$WORK/poppler/utils/$program" "$OUT/"
+done
+
+for program in "${poppler_BUILD_PROGRAMS[@]}"; do
+    if [ "$program" = pdf_fuzzer ]; then
+        $CXX $CXXFLAGS -std=c++11 -I"$WORK/poppler/cpp" -I"$TARGET/repo/cpp" \
+            "$TARGET/src/pdf_fuzzer.cc" -o "$OUT/pdf_fuzzer" \
+            "$WORK/poppler/cpp/libpoppler-cpp.a" "$WORK/poppler/libpoppler.a" \
+            "$WORK/lib/libfreetype.a" $LDFLAGS $LIBS -ljpeg -lz \
+            -lopenjp2 -lpng -ltiff -llcms2 -lm -lpthread -pthread
+    fi
+done
