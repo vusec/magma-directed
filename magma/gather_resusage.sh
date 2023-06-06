98084cb7fd2d52c19eb5e44e7e4c17f8f0fdc226
Andrea Jemmett <1787979+acidghost@users.noreply.github.com>
2023-06-06 09:33:22 +0200

    feat: gather_resusage.sh

---
+++ magma/gather_resusage.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+# Run given executable and print time and memory usage
+
+if [ $# -lt 1 ]; then
+    echo "usage: $0 <executable> [args...]"
+    exit 1
+fi
+
+/usr/bin/time -v "$@" &
+pid=$!
+
+max_mem=0
+while [ -d /proc/$pid ]; do
+    mem=$(free -t | grep Total | awk '{print $3}')
+    if [ "$mem" -gt $max_mem ]; then
+        max_mem=$mem
+    fi
+    sleep 5
+done
+wait $pid
+printf '\n\nMax memory usage: %d\n' "$max_mem"
