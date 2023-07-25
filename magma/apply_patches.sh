37ae75bef76bac3feea91d7645d673e7f4bcfe4a
Andrea Jemmett <1787979+acidghost@users.noreply.github.com>
2023-07-25 17:44:04 +0200

    fix(magma): apply_patches sorted or MAGMA_BUG last
    If MAGMA_BUG is not applied last line numbers will not be correct and
    will mess up directed fuzzing. Now applies the patches correctly and in
    a consistent order.

--- magma/apply_patches.sh
+++ magma/apply_patches.sh
@@ -8,9 +8,11 @@ set -e
 
 cd "$TARGET/repo"
 
+# configure git to create commits
 git config user.name &>/dev/null || git config --global user.name magma
 git config user.email &>/dev/null || git config --global user.email '<>'
 
+# initialize git repo if needed
 if [ ! -d .git ]; then
     git init
     git add .
@@ -21,58 +23,69 @@ tag=magma-init
 echo "[+] Tagging initial git state with $tag"
 git tag $tag
 
-search_dirs=()
+find_patches() { find "$1" -name "*.patch" -print0 | sort -z; }
+patch_name() { local name=${1##*/}; echo "${name%.patch}"; }
+
+# apply setup patches
+has_setup_patches=0
+setup_tag=magma-setup
 setup_dir="$TARGET/patches/setup"
 if [ -d "$setup_dir" ]; then
-    search_dirs+=("$setup_dir")
+    while read -r -d '' patch; do
+        has_setup_patches=1
+        echo "[+] Applying setup $patch"
+        patch -p1 --no-backup-if-mismatch <"$patch"
+    done < <(find_patches "$setup_dir")
 fi
+
+if [ $has_setup_patches -eq 1 ]; then
+    echo "[+] Setup done, tagging as $setup_tag"
+    git add .
+    git commit -m $setup_tag
+else
+    echo "[-] No setup patches, tagging as $setup_tag"
+fi
+
+git tag $setup_tag
+
+# apply bug patches
 bugs_dir="$TARGET/patches/bugs"
 if [ ! -d "$bugs_dir" ]; then
     echo >&2 "[!] There is no $bugs_dir directory"
     exit 1
 fi
-search_dirs+=("$bugs_dir")
 
-is_setup=1
-has_setup_patches=0
-setup_tag=magma-setup
+if [ -n "$MAGMA_BUG" ] && [ ! -f "$bugs_dir/$MAGMA_BUG.patch" ]; then
+    echo >&2 "[!] There is no $bugs_dir/$MAGMA_BUG.patch file"
+    exit 1
+fi
 
-# TODO filter patches by target config.yaml
-while read -r patch; do
-    kind=$(basename "$(dirname "$patch")")
-    if [ "$kind" = setup ]; then
-        has_setup_patches=1
+# sort patches by name and place the target patch at the end
+bugs=()
+while read -r -d '' patch; do
+    if [ -z "$MAGMA_BUG" ] || [ "$(patch_name "$patch")" != "$MAGMA_BUG" ]; then
+        bugs+=("$patch")
     fi
-    if [ "$kind" = bugs ] && [ $is_setup -eq 1 ]; then
-        # this is the first bug patch, setup is finished
-        is_setup=0
-        tag=$setup_tag
-        if [ $has_setup_patches -eq 1 ]; then
-            echo "[+] Setup done, tagging as $tag"
-            git add .
-            git commit -m $tag
-        else
-            echo "[-] No setup patches, tagging as $tag"
-        fi
-        git tag $tag
-    fi
+done < <(find_patches "$bugs_dir")
 
-    echo "[+] Applying $patch"
-    name=${patch##*/}
-    name=${name%.patch}
+if [ -n "$MAGMA_BUG" ]; then
+    bugs+=("$bugs_dir/$MAGMA_BUG.patch")
+fi
+
+for patch in "${bugs[@]}"; do
+    echo "[+] Applying bug $patch"
+    name=$(patch_name "$patch")
     sed "s/%MAGMA_BUG%/$name/g" "$patch" | patch -p1 --no-backup-if-mismatch
-
-    if [ "$kind" = bugs ]; then
-        oldtag=$tag
-        tag="magma-bug-$name"
-        echo "[+] Tagging $tag"
-        git add .
-        git commit -m "$tag"
-        git tag "$tag"
+    tag="magma-bug-$name"
+    echo "[+] Tagging $tag"
+    git add .
+    git commit -m "$tag"
+    git tag "$tag"
+    if [ -n "$MAGMA_BUG" ] && [ "$name" = "$MAGMA_BUG" ]; then
         echo "[+] Storing $OUT/bug_$name.diff"
-        git diff "$oldtag..$tag" >"$OUT/bug_$name.diff"
+        git diff "$tag^..$tag" >"$OUT/bug_$name.diff"
     fi
-done < <(find "${search_dirs[@]}" -name "*.patch")
+done
 
 echo "[+] Storing total diff of patches into $OUT/bugs.diff"
 git diff $setup_tag..HEAD >"$OUT/bugs.diff"
