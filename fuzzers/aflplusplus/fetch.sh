5a5fcf50d33a86c096cf3da3180cb359060c49a5
Andrea Jemmett <1787979+acidghost@users.noreply.github.com>
2023-08-29 16:55:02 +0200

    feat(aflplusplus): proper persistent fuzzing
    Allow full persistent fuzzing with shared memory for test cases.
    Also add weak definition of LLVMFuzzerTestOneInput to allow compiling
    poppler.

--- fuzzers/aflplusplus/fetch.sh
+++ fuzzers/aflplusplus/fetch.sh
@@ -9,4 +9,6 @@ set -e
 git clone --no-checkout https://github.com/AFLplusplus/AFLplusplus "$FUZZER/repo"
 git -C "$FUZZER/repo" checkout v4.08c
 
-sed -i 's/\(__afl_sharedmem_fuzzing\) = 1/\1 = 0/' "$FUZZER/repo/utils/aflpp_driver/aflpp_driver.c"
+sed -i '80,81c\
+__attribute__((weak)) int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) { return 0; }
+' "$FUZZER/repo/utils/aflpp_driver/aflpp_driver.c"
