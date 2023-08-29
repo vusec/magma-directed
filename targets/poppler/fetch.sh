e55e061d5c7b39ce82e7301b295fdac8c291b448
Andrea Jemmett <1787979+acidghost@users.noreply.github.com>
2023-08-29 16:57:51 +0200

    fix(poppler): exit if fetching freetype fails

--- targets/poppler/fetch.sh
+++ targets/poppler/fetch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 
 ##
 # Pre-requirements:
@@ -10,5 +11,5 @@ git clone --no-checkout https://gitlab.freedesktop.org/poppler/poppler.git \
 git -C "$TARGET/repo" checkout 1d23101ccebe14261c6afc024ea14f29d209e760
 
 git clone --no-checkout https://gitlab.freedesktop.org/freetype/freetype.git \
-	"$TARGET/freetype2"
+    "$TARGET/freetype2"
 git -C "$TARGET/freetype2" checkout 50d0033f7ee600c5f5831b28877353769d1f7d48
