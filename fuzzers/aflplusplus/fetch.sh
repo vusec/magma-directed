f75125e314b01df8e111ca74dd150d8dd3dece6d
Andrea Jemmett <1787979+acidghost@users.noreply.github.com>
2023-09-08 16:30:53 +0200

    fix(aflplusplus): cmplog fsrv restart on php

--- fuzzers/aflplusplus/fetch.sh
+++ fuzzers/aflplusplus/fetch.sh
@@ -9,6 +9,10 @@ set -e
 git clone --no-checkout https://github.com/AFLplusplus/AFLplusplus "$FUZZER/repo"
 git -C "$FUZZER/repo" checkout v4.08c
 
+# Fix for https://github.com/AFLplusplus/AFLplusplus/issues/1845
+curl -sSL https://github.com/AFLplusplus/AFLplusplus/commit/5f6c76e192bcfde6abcf9d4156bfbb87d5480e23.diff \
+    | git -C "$FUZZER/repo" apply
+
 sed -i '80,81c\
 __attribute__((weak)) int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) { return 0; }
 ' "$FUZZER/repo/utils/aflpp_driver/aflpp_driver.c"
