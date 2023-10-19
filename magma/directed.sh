b65287139beafb2d4e1f01ce8582238cb706f848
Andrea Jemmett <1787979+acidghost@users.noreply.github.com>
2023-10-20 00:36:36 +0200

    feat(magma): make_magma_log_lines_unique w/ TIF012

--- magma/directed.sh
+++ magma/directed.sh
@@ -1,6 +1,5 @@
 # shellcheck shell=bash disable=SC2034
 
-
 store_target_lines() {
     if [ ! -f "$MAGMA_BUG_FILE" ]; then
         printf >&2 "MAGMA_BUG_FILE=%q is not a file\n" "$MAGMA_BUG_FILE"
@@ -21,14 +20,37 @@ store_target_lines() {
     fi
 }
 
-store_magma_log_lines() {
+check_env_magma_bug() {
     if [ -z "$MAGMA_BUG" ]; then
         printf >&2 "MAGMA_BUG is not set\n"
         return 1
     fi
+}
+
+store_magma_log_lines() {
+    check_env_magma_bug || return 1
 
     local file=${1:-"$OUT/directed_targets.txt"}
     grep -rIn 'MAGMA_LOG("'"$MAGMA_BUG" "$TARGET/repo" \
         | sed -E 's/^(.+:[0-9]+):.*$/\1/' \
         | sort -u >"$file"
+}
+
+make_magma_log_lines_unique() {
+    check_env_magma_bug || return 1
+
+    local target_name=$1
+    local infile=${2:-"$OUT/directed_targets.txt"}
+    (
+        case ${1} in
+        libtiff)
+            case ${MAGMA_BUG} in
+            TIF012)
+                exec grep -F 'tif_dir.c:313' "$infile"
+                ;;
+            esac
+            ;;
+        esac
+        cat "$infile"
+    )
 }
