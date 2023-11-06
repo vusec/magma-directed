5282645421843f2470dc07b389d917f083dee821
Andrea Jemmett <1787979+acidghost@users.noreply.github.com>
2023-11-06 17:53:58 +0100

    fix(libafl): dont_optimize

--- fuzzers/libafl/fetch.sh
+++ fuzzers/libafl/fetch.sh
@@ -8,3 +8,10 @@ set -ex
 
 mkdir -m 0700 ~/.ssh && ssh-keyscan github.com >> ~/.ssh/known_hosts
 git clone --depth 1 git@github.com:vusec/LibAFL-directed.git "$FUZZER/repo"
+
+cd "$FUZZER/repo"
+sed -i '30c\
+            .silence(env::var("LIBAFL_CC_VERBOSE").is_err())\
+            // Honor -O0 flag\
+            .dont_optimize()
+' fuzzers/fuzzbench/src/bin/libafl_cc.rs
