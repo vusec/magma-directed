2dccf2d9688a57231307a09d52bf92143eb3d259
Andrea Jemmett <1787979+acidghost@users.noreply.github.com>
2023-11-06 17:37:58 +0100

    feat(libaflgo): update commit

--- fuzzers/libaflgo_aflgo/fetch.sh
+++ fuzzers/libaflgo_aflgo/fetch.sh
@@ -8,4 +8,4 @@ set -ex
 
 mkdir -m 0700 ~/.ssh && ssh-keyscan github.com >> ~/.ssh/known_hosts
 git clone --no-checkout git@github.com:vusec/aflgo-new.git "$FUZZER/repo"
-git -C "$FUZZER/repo" checkout e284e46ffa2d56d93ebfccccd5892d1a5f14a52d
+git -C "$FUZZER/repo" checkout 351fc8d96f3d4a22813a44634a50bff168b2b301
