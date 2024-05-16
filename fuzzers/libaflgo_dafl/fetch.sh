065b68276155fe4aa033af0909124a098a6a1272
Andrea Jemmett <1787979+acidghost@users.noreply.github.com>
2024-05-16 19:56:16 +0200

    fix(libaflgo_dafl): wrapper for ASM

--- fuzzers/libaflgo_dafl/fetch.sh
+++ fuzzers/libaflgo_dafl/fetch.sh
@@ -8,4 +8,4 @@ set -ex
 
 mkdir -m 0700 ~/.ssh && ssh-keyscan github.com >> ~/.ssh/known_hosts
 git clone --no-checkout git@github.com:vusec/aflgo-new.git "$FUZZER/repo"
-git -C "$FUZZER/repo" checkout e358aef18e2f8b1f4c1092b4ed1fd1f2fc7cc348
+git -C "$FUZZER/repo" checkout 877834a1cd075dee708f8888e9b3fc144e090853
