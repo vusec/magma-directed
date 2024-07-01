7f5a0bbb668007ad0d71e5db89b046bfb146aa39
Andrea Jemmett <1787979+acidghost@users.noreply.github.com>
2024-07-01 12:26:19 +0200

    fix(libaflgo): fix dependencies for Rust version

--- fuzzers/libaflgo_hawkeye/fetch.sh
+++ fuzzers/libaflgo_hawkeye/fetch.sh
@@ -8,4 +8,4 @@ set -ex
 
 mkdir -m 0700 ~/.ssh && ssh-keyscan github.com >> ~/.ssh/known_hosts
 git clone --no-checkout git@github.com:vusec/aflgo-new.git "$FUZZER/repo"
-git -C "$FUZZER/repo" checkout b5e054930b55e65b2b5c43b9643d69b3e52479b7
+git -C "$FUZZER/repo" checkout 5df44ab2f462440cd4463184a56d9e138bd095c3
